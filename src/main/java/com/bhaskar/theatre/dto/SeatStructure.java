dd840e3b79d835269cb95b142e02796646afe623
sarthaksarthak9 <sarthaknegi908@gmail.com>
2026-02-17 23:16:53 +0530

    seat structure

---
+++ src/main/java/com/bhaskar/theatre/dto/SeatStructure.java
@@ -0,0 +1,11 @@
+package com.bhaskar.theatre.dto;
+
+import lombok.Data;
+
+@Data
+public class SeatStructure {
+    private int seatCount;
+    private double seatPrice;
+    private String area;
+}
+
