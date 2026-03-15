4da837d753db1c7501870a1e7508817cff926573
sarthaksarthak9 <sarthaknegi908@gmail.com>
2026-03-15 23:23:59 +0530

    movie

---
+++ src/main/java/com/bhaskar/theatre/repository/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.bhaskar.theatre.repository;
+
+import com.bhaskar.theatre.entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+}
