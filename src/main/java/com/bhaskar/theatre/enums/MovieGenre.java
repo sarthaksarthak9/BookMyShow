d71167e5c30bd6871eb9708447d7d186afada46b
sarthaksarthak9 <sarthaknegi908@gmail.com>
2026-03-15 23:22:25 +0530

    enum

---
+++ src/main/java/com/bhaskar/theatre/enums/MovieGenre.java
@@ -0,0 +1,12 @@
+package com.bhaskar.theatre.enums;
+
+public enum MovieGenre {
+    ACTION,
+    THRILLER,
+    HORROR,
+    COMEDY,
+    SUSPENSE,
+    DRAMA,
+    ROMANCE,
+    SCIENCE_FICTION
+}
